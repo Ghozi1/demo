e9d564d4da01a917b777698bc89b14bf666dd1af
Muhammad Ghozi Al Ghifari <42824657+Ghozi1@users.noreply.github.com>
2023-08-04 09:55:23 +0700

    method foorlooping

---
+++ src/main/java/pertermuan_pertama_paw/ForLoop.java
@@ -0,0 +1,9 @@
+package pertermuan_pertama_paw;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (;;){
+            System.out.println("Perulangan Tanpa Henti");
+        }
+    }
+}
