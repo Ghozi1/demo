fdafd6e9c7fec2998a0b3dd6440ef6c850eddfed
Muhammad Ghozi Al Ghifari <42824657+Ghozi1@users.noreply.github.com>
2023-08-04 10:00:49 +0700

    foor lopp dengan kondisi init statement

---
+++ src/main/java/pertermuan_pertama_paw/ForLoop_DenganInitState.java
@@ -0,0 +1,10 @@
+package pertermuan_pertama_paw;
+
+public class ForLoop_DenganInitState {
+    public static void main(String[] args) {
+        for(var counter = 1; counter <=10;){
+            System.out.println("Perulangan Ke-" + counter);
+            counter++;
+        }
+    }
+}
