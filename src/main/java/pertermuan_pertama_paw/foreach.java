0b5a89696297f4ba48b5a45c16c21a881268bacd
Muhammad Ghozi Al Ghifari <42824657+Ghozi1@users.noreply.github.com>
2023-08-04 10:35:17 +0700

    for each

---
+++ src/main/java/pertermuan_pertama_paw/foreach.java
@@ -0,0 +1,13 @@
+package pertermuan_pertama_paw;
+
+public class foreach {
+    public static void main(String[] args) {
+        String[] array = {
+            "Suep","Paijo","Sukinem","memet"
+        };
+
+        for(int i = 0; i< array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
