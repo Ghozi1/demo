1aba64e109d3145b348bef0cf51380e1b09e761a
Muhammad Ghozi Al Ghifari <42824657+Ghozi1@users.noreply.github.com>
2023-08-04 10:15:41 +0700

    dowhile forloop

---
+++ src/main/java/pertermuan_pertama_paw/dowhileloop.java
@@ -0,0 +1,12 @@
+package pertermuan_pertama_paw;
+
+public class dowhileloop {
+    public static void main(String[] args) {
+        var counter = 100;
+        
+        do{
+            System.out.println("Perulangan Ke -10" + counter);
+            counter++;
+        }while(counter <=10);
+    }
+}
