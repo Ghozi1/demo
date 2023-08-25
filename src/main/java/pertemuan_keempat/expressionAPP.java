e724dd30a6a78621b7904bea5684fdd21828401d
Muhammad Ghozi Al Ghifari <42824657+Ghozi1@users.noreply.github.com>
2023-08-25 23:13:20 +0700

    zz

---
+++ src/main/java/pertemuan_keempat/expressionAPP.java
@@ -0,0 +1,11 @@
+package pertemuan_keempat;
+
+public class expressionAPP {
+     //code block
+     public static void main(String[] args) {
+        double avalue = 8933.234;
+        avalue++;
+        System.out.println("hello word");
+        Date date = new Date(0);
+    }
+}
